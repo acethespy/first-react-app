5b932e22ff2e89503d84852e7b138c4da6c4560f
Joe Zou <joezou@berkeley.edu>
2020-06-16 13:34:35 -0700

    Updates

---
+++ precache-manifest.4739d0a85ea703e61b65c2a4e40833d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61072b763bce31009863eba91ee8b014",
+    "url": "/first-react-app/index.html"
+  },
+  {
+    "revision": "5a64c24a8e4f2625c7b7",
+    "url": "/first-react-app/static/css/main.efca1749.chunk.css"
+  },
+  {
+    "revision": "36207a6f59af3c5ec409",
+    "url": "/first-react-app/static/js/2.361708d4.chunk.js"
+  },
+  {
+    "revision": "29c8d19450d117745c250b1131e2dada",
+    "url": "/first-react-app/static/js/2.361708d4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5a64c24a8e4f2625c7b7",
+    "url": "/first-react-app/static/js/main.445bad3b.chunk.js"
+  },
+  {
+    "revision": "2ce4251fe5ca2257e294",
+    "url": "/first-react-app/static/js/runtime-main.7eebba03.js"
+  }
+]);
